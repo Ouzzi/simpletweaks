9f288a1840bef5c3fecb0aa4ec43792fb34bc5da
Oussama Azarfane <62378600+Ouzzi@users.noreply.github.com>
2026-01-03 17:37:20 +0100

    ecocompass custom recipe added

--- src/main/java/com/simpletweaks/datagen/ModRecipeProvider.java
+++ src/main/java/com/simpletweaks/datagen/ModRecipeProvider.java
@@ -5,13 +5,20 @@ import com.simpletweaks.block.ModBlocks;
 import com.simpletweaks.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.recipe.SmithingTransformRecipeJsonBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
@@ -30,30 +37,23 @@ public class ModRecipeProvider extends FabricRecipeProvider {
             public void generate() {
                 // Smithing Rezept: Netherite Upgrade + Gold Pressure Plate = Spawn Teleporter
                 SmithingTransformRecipeJsonBuilder.create(
-                                // Template (Upgrade Template)
                                 Ingredient.ofItems(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE),
-                                // Base (Gold Pressure Plate)
                                 Ingredient.ofItems(Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE),
-                                // Addition (Netherite Ingot)
                                 Ingredient.ofItems(Items.NETHERITE_INGOT),
                                 RecipeCategory.TOOLS,
                                 ModBlocks.SPAWN_TELEPORTER.asItem()
                         )
-                        // hasItem und conditionsFromItem sind Methoden von RecipeGenerator und hier verfügbar
                         .criterion(hasItem(Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE), conditionsFromItem(Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE))
                         .offerTo(exporter, getRecipeName(ModBlocks.SPAWN_TELEPORTER) + "_smithing");
 
+                // Smithing Rezept: Launchpad
                 SmithingTransformRecipeJsonBuilder.create(
-                                // Template (Upgrade Template)
                                 Ingredient.ofItems(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE),
-                                // Base (Gold Pressure Plate)
                                 Ingredient.ofItems(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE),
-                                // Addition (Netherite Ingot)
                                 Ingredient.ofItems(Items.NETHERITE_INGOT),
                                 RecipeCategory.TOOLS,
                                 ModBlocks.LAUNCHPAD.asItem()
                         )
-                        // hasItem und conditionsFromItem sind Methoden von RecipeGenerator und hier verfügbar
                         .criterion(hasItem(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE), conditionsFromItem(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE))
                         .offerTo(exporter, getRecipeName(ModBlocks.LAUNCHPAD) + "_smithing");
             }
