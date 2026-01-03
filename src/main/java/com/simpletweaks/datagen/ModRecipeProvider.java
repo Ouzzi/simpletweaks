cf6caea0685dd1746e31d974ef7c3d84b4d2bbf1
Oussama Azarfane <62378600+Ouzzi@users.noreply.github.com>
2026-01-03 17:48:05 +0100

    ecocompass custom recipe added

--- src/main/java/com/simpletweaks/datagen/ModRecipeProvider.java
+++ src/main/java/com/simpletweaks/datagen/ModRecipeProvider.java
@@ -1,26 +1,16 @@
 package com.simpletweaks.datagen;
 
-import com.simpletweaks.Simpletweaks;
 import com.simpletweaks.block.ModBlocks;
-import com.simpletweaks.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.recipe.SmithingTransformRecipeJsonBuilder;
-import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -46,7 +36,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                         .criterion(hasItem(Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE), conditionsFromItem(Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE))
                         .offerTo(exporter, getRecipeName(ModBlocks.SPAWN_TELEPORTER) + "_smithing");
 
-                // Smithing Rezept: Launchpad
+                // Smithing Rezept: Netherite Upgrade + Heavy Pressure Plate = Launchpad
                 SmithingTransformRecipeJsonBuilder.create(
                                 Ingredient.ofItems(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE),
                                 Ingredient.ofItems(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE),
